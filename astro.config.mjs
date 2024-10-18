148add2ea268f063d76045c3e93af5026f557321
casoon <joern.seidel@casoon.de>
2024-10-18 12:24:55 +0200

    direct webp upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
     output: 'server',
     adapter: cloudflare({
-        imageService: 'compile'
-    }),
-    image: {
-        service: {
-            entrypoint: 'astro/assets/services/sharp',
-            config: {
-                limitInputPixels: false,
-            },
-        },
-    },
+        imageService: 'passthrough'
+    })
 });
